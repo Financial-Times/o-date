44c3bf9790bb24b3923c42a95e2cd1d9ea083130
Maria Benavente <mariabenaventeg@gmail.com>
2016-08-24 16:25:57 +0100

    content generated from the .js file

--- demos/src/formatOptions.js
+++ demos/src/formatOptions.js
@@ -1,4 +1,4 @@
-const times = document.querySelectorAll('[data-o-component="o-date"]');
+const dates = document.querySelectorAll('[data-o-component="o-date"]');
 const now = new Date();
 const recently = new Date();
 const today = new Date();
@@ -9,16 +9,29 @@ today.setHours(now.getHours() - 6);
 yesterday.setHours(now.getHours() - 24);
 daysAgo.setDate(now.getDay() - 2);
 
-let moments = [now, recently, today, yesterday, daysAgo];
+const formats = ['Publish Date', 'Default o-date output', 'time-ago-limit-4-hours output', 'date-only output', 'as-today-or-yesterday-or-nothing output'];
+const moments = [now, recently, today, yesterday, daysAgo];
+const momentNames = ['Now', 'Recently', 'Earlier today', 'Yesterday', 'Few Days Ago', 'Long Ago'];
+const publishDates = 	document.getElementsByClassName('publish-dates');
 
 // Current browser time displayed
 document.getElementById("current-date").innerHTML = now.toISOString();
+
+for (let i = 0; i<formats.length; i++) {
+	let th = document.createElement('th');
+	th.appendChild(document.createTextNode(formats[i]));
+	document.getElementsByTagName("thead")[0].appendChild(th);
+}
+for (let i = 0; i<publishDates.length; i++) {
+	let td = publishDates[i];
+	td.appendChild(document.createTextNode(momentNames[i]));
+}
 
 // declarative mode used
 let c = 0;
 for (let i = 1; i < moments.length; i++) {
 	for (let j = 0; j < 4; j++) {
-		times[c].setAttribute('datetime', moments[i].toISOString());
+		dates[c].setAttribute('datetime', moments[i].toISOString());
 		c++;
 	}
 }
