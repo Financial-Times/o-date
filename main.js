bdf4208a574a1ac661c5643e5a53838a110e81bc
chee <chee@snoot.club>
2021-10-06 18:01:26 +0100

    Add note about The Origami Component System

--- main.js
+++
@@ -1,7 +0,0 @@
-import oDate from './src/js/date.js';
-const constructAll = function () {
-	oDate.init();
-	document.removeEventListener('o.DOMContentLoaded', constructAll);
-};
-document.addEventListener('o.DOMContentLoaded', constructAll);
-export default oDate;
